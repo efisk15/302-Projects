51ae44e3092b0bc8df9d7acb69cf8963863930fb
Eli Fisk <mister.e.fisk@gmail.com>
2025-02-13 08:56:16 -0500

    My Updates

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,7 +1,28 @@
 // Challenge 06: Repeated DNA sequences
-
+#include <iostream>
+#include <map>
+using namespace std;
 int main(int argc, char *argv[]) {
+    string input;
+    while(cin >> input) {
+        int i = 0;
+        map<string, bool> answers;
+        while ((i + 8) < int(input.size())) {
+            string val = input.substr(i, 9);
+            if(answers.find(val) == answers.end()) {
+                answers.insert({val, false});
+            } else {
+                answers[val] = true;
+            }
+            i++;
+        }
+        map<string, bool>::iterator answer;
+        for(answer = answers.begin(); answer != answers.end(); ++answer) {
+            if(answer->second) {
+                cout << answer->first << endl;
+            }
+        }
+        cout << -1 << endl;
+    }
     return 0;
 }
-
-
